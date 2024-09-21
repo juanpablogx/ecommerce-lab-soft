383db26064986bdfc877205c821fa7608d4a8c17
SMejia115 <s.mejia6@utp.edu.co>
2024-09-20 20:53:36 -0500

    Usuarios initial CRUD

---
+++ backend/src/usuarios/usuarios.controller.js
@@ -0,0 +1,65 @@
+const services = require('./usuarios.service');
+
+const createController = async (req, res) => {
+  try {
+    const usuario = req.body;
+    const newUsuario = await services.create(usuario);
+    res.status(201).json(newUsuario);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const findAllController = async (req, res) => {
+  try {
+    const usuarios = await services.findAll();
+    res.status(200).json(usuarios);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const findByIdController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const usuario = await services.findById(id);
+    if (!usuario) {
+      return res.status(404).json({ message: `Usuario con id ${id} no encontrado` });
+    }
+    res.status(200).json(usuario);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const updateController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const usuario = req.body;
+    const updatedUsuario = await services.update(id, usuario);
+    res.status(200).json(updatedUsuario);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const removeController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const row_affected = await services.remove(id);
+    if (row_affected === 0) {
+      return res.status(404).json({ message: `Usuario con id ${id} no encontrado` });
+    }
+    res.status(200).json({ message: `Usuario con id ${id} eliminado` });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+module.exports = {
+  createController,
+  findAllController,
+  findByIdController,
+  updateController,
+  removeController,
+};
---
+++ backend/src/usuarios/usuarios.service.js
@@ -0,0 +1,38 @@
+const Usuario = require('./usuario.model');
+
+const create = async (usuario) => {
+  return await Usuario.create(usuario);
+};
+
+const findAll = async () => {
+  return await Usuario.findAll();
+};
+
+const findById = async (id) => {
+  return await Usuario.findByPk(id);
+};
+
+const update = async (id, usuario) => {
+  return await Usuario.update(usuario, {
+    where: {
+      id_usuario: id,
+    },
+    returning: true, // Para devolver el registro actualizado
+  });
+};
+
+const remove = async (id) => {
+  return await Usuario.destroy({
+    where: {
+      id_usuario: id,
+    },
+  });
+};
+
+module.exports = {
+  create,
+  findAll,
+  findById,
+  update,
+  remove,
+};
