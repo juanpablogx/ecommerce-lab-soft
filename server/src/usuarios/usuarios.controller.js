833858f0784b16287076baf90612ae407d6c6a4b
mcabranchiz <m.cabrera@utp.edu.co>
2024-10-13 20:43:27 -0500

    Add files via upload

--- server/src/usuarios/usuarios.controller.js
+++ server/src/usuarios/usuarios.controller.js
@@ -1,5 +1,8 @@
 const services = require('./usuarios.service');
+const services = require('./usuarios.service');
+const bcrypt = require('bcrypt');
 const jwt = require('../helpers/jwt');
+
 
 const createController = async (req, res) => {
   try {
@@ -57,19 +60,43 @@ const removeController = async (req, res) => {
   }
 };
 
-const loginController = async (req, res) => {
+const registerController = async (req, res) => {
   try {
-    const { correo, password } = req.body;
-    const usuario = await services.findByEmailPassword(correo, password);
-    if (!usuario) {
-      return res.status(404).json({ message: 'Usuario no encontrado' });
+    const { nombre_usuario, apellido_usuario, correo_usuario, telefono_usuario, direccion_usuario, password_usuario, rol_usuario } = req.body;
+
+    
+    if (!nombre_usuario || !apellido_usuario || !correo_usuario || !password_usuario || !rol_usuario) {
+      return res.status(400).json({ message: 'Todos los campos son requeridos' });
     }
-    const token = jwt.generateAccessToken(usuario);
-    res.status(200).json({ token, usuario: { ...usuario.get(), password_usuario: undefined } });
+
+    
+    const usuarioExistente = await services.findByEmail(correo_usuario);
+    if (usuarioExistente) {
+      return res.status(400).json({ message: 'El usuario ya existe con este correo' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password_usuario, salt);
+
+    const nuevoUsuario = await services.create({
+      nombre_usuario,
+      apellido_usuario,
+      correo_usuario,
+      telefono_usuario,
+      direccion_usuario,
+      password_usuario: hashedPassword,
+      rol_usuario
+    });
+
+  
+    const token = jwt.generateAccessToken(nuevoUsuario);
+
+    res.status(201).json({ message: 'Usuario registrado con éxito', token, usuario: { ...nuevoUsuario.get(), password_usuario: undefined } });
   } catch (error) {
-    res.status(400).json(error);
+    console.error(error);
+    res.status(500).json({ message: 'Error en el servidor', error });
   }
-};
+}
 
 module.exports = {
   createController,
@@ -77,5 +104,5 @@ module.exports = {
   findByIdController,
   updateController,
   removeController,
-  loginController
+  registerController
 };
