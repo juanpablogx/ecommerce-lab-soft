9bcd76cb2c17f8f20d43beb5f38df72934e810c3
SMejia115 <s.mejia6@utp.edu.co>
2024-10-25 13:58:35 -0500

    bug correction

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'product-details/:id_producto', component: ProductDetailsComponent}
+  {path: 'product-details/:id_producto', component: ProductDetailsComponent},
   {path: 'home', component: MainHomeComponent}
 ];
 
