5fe16f48a9b6cce39ca6676e4463ab5af9648388
SMejia115 <s.mejia6@utp.edu.co>
2024-11-20 16:17:20 -0500

    fix(front): Login redirectin bug

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -3,25 +3,38 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { EcommerceService } from '../../services/ecommerce.service';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
 
-  constructor(private router: Router, private http: HttpClient, private ecommerceService: EcommerceService) {}
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private ecommerceService: EcommerceService
+  ) {}
 
   onSubmit() {
-    this.ecommerceService.login({correo: this.username, password: this.password}).subscribe((data:any) => {
-      console.log('Login exitoso:', data);
-      window.alert('Login exitoso.');
-      localStorage.setItem('token', data.token);
-      this.router.navigate(['/home']);
-    }
-    )
+    this.ecommerceService
+      .login({ correo: this.username, password: this.password })
+      .subscribe(
+        (data: any) => {
+          console.log('Login exitoso:', data);
+          window.alert('Login exitoso.');
+          localStorage.setItem('token', data.token);
+          this.router.navigate(['']).then(() => {
+            // Recargar la página para asegurar los cambios (si es necesario)
+            window.location.reload();
+          });
+        },
+        (error) => {
+          console.error('Error en el login:', error);
+          window.alert('Error en el login. Verifica tus credenciales.');
+        }
+      );
   }
 }
